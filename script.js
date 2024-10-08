1f0e1ac2e5dca8b1d0b6ac0a6bfbdec7f25660d2
Zorro246 <hellozoro4@gmail.com>
2024-10-08 17:24:11 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+function appendToDisplay(value) {
+    const display = document.getElementById('display');
+    display.value += value;
+}
+
+function clearDisplay() {
+    const display = document.getElementById('display');
+    display.value = '';
+}
+
+function deleteLast() {
+    const display = document.getElementById('display');
+    display.value = display.value.slice(0, -1);
+}
+
+function calculate() {
+    const display = document.getElementById('display');
+    try {
+        // Check if there's any value to calculate
+        if (display.value === '') {
+            display.value = '';
+        } else {
+            display.value = eval(display.value);
+        }
+    } catch (error) {
+        display.value = 'Error';
+    }
+}
